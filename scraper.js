883fc7fe9cef07878f7e39e75c4854be020beb54
RMRAmadushanka <rmramadushanka@gmail.com>
2024-10-14 22:04:36 +0530

    Initialized Puppeteer browser with headless configuration

--- scraper.js
+++ scraper.js
@@ -1,5 +1,11 @@
 const puppeteer = require('puppeteer');
 
 const scrapeIkman = async (keyword) => {
-    
+    const browser = await puppeteer.launch({
+        headless: true, // Run headless
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Ensure Puppeteer runs without issues
+    });
+
+    const page = await browser.newPage();
+
 }
